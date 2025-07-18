c7bb580260dc748d5a01a6fb781c24ea5e06c4ee
ignacio <ig.mugonz@gmail.com>
2025-07-17 21:45:43 -0400

    1st

---
+++ tests/first_test.py
@@ -0,0 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+def test_page_title():
+    browser = webdriver.Chrome(service=Service("C:\\Users\\igmug\\chromedriver-win64\\chromedriver.exe"))
+    browser.get('https://github.com')
+
+    titleElement = browser.find_element(By.ID,'hero-section-brand-heading')
+
+    assert titleElement.text == 'Build and ship software on a single, collaborative platform'
+
+
+    browser.quit()
+
+
